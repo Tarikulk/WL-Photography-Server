7a13ba933c1c167885e9e1d93d3d2efd2543aabb
Tarikulk <ullahtarik300@gmail.com>
2022-11-08 14:51:29 +0600

    mongodb added

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-
+require("dotenv").config();
+ 
 app.use(cors());
 app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mtnim2c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 
 
 app.get("/", (req, res) =>{
@@ -14,3 +23,4 @@ app.get("/", (req, res) =>{
 app.listen(port, () =>{
     console.log(`This server is running on port ${port}`);
 });
+ 
