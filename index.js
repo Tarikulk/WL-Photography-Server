2ceee58448544026a6c9258f71741648b2ffbe93
Tarikulk <ullahtarik300@gmail.com>
2022-11-08 16:58:44 +0600

    server services endpoint created

--- index.js
+++ index.js
@@ -10,9 +10,27 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mtnim2c.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run (){
+    try{
+       const servicesCollection = client.db("servicesReview").collection("services"); 
 
+              app.get("/services", async(req, res) =>{
+               const query = {};
+               const cursor = servicesCollection.find(query);
+               const result = await cursor.toArray();
+               res.send(result);
+              })
+           }
+       finally{
+
+    }
+}
+
+run().catch(error => console.error(error));
 
 
 
