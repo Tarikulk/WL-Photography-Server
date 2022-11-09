c8a70732b952e0f1cd631c149eb448b2ce32185c
Tarikulk <ullahtarik300@gmail.com>
2022-11-09 20:20:56 +0600

    delete functionality again added

--- index.js
+++ index.js
@@ -64,6 +64,15 @@ app.get("/reviews", async(req, res) =>{
     
 }) 
 
+
+app.delete("/reviews/:id", async(req, res) =>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const result = await reviewCollection.deleteOne(query);
+    res.send(result);
+})
+
+
            }
        finally{
 
