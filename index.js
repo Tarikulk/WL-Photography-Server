cadb68221656a92573635aff416b7828de499072
Tarikulk <ullahtarik300@gmail.com>
2022-11-09 20:16:14 +0600

    all services added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -8,22 +9,28 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mtnim2c.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run (){
     try{
        const servicesCollection = client.db("servicesReview").collection("services"); 
+       const reviewCollection = client.db("servicesReview").collection("reviews");
 
               app.get("/services", async(req, res) =>{
                const query = {};
                const cursor = servicesCollection.find(query);
-               const result = await cursor.toArray();
+               const result = await cursor.limit(3).toArray();
                res.send(result);
-              })
+              });
+
+              app.get("/allServices", async(req, res) =>{
+                const query = {};
+                const cursor = servicesCollection.find(query);
+                const result = await cursor.toArray();
+                res.send(result);
+               });
+
            }
        finally{
 
